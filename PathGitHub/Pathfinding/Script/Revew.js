9d7353900154d94b53c8b4257d0e0ee24f7d6c35
John Doe <johndoe@email.com>
2024-04-17 13:05:12 +0200

    Review sida

---
+++ PathGitHub/Pathfinding/Script/Revew.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    let DarkModeHandler = document.querySelectorAll(".darkMode");
+
+    if(localStorage.getItem("darkMode") == null){
+        localStorage.setItem("darkMode", 1);
+        
+        for(let x = 0; x <= DarkModeHandler.length; x++){
+            DarkModeHandler[x].classList.toggle("darkMode");
+        }
+
+    }
+    else if(localStorage.getItem("darkMode") == 1){
+
+        for(let x = 0; x < DarkModeHandler.length; x++){
+            DarkModeHandler[x].classList.toggle("darkMode");
+        }
+    }
+
+
+
+
+
+
+
+    
+});
