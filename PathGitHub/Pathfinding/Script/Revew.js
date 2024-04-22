d39004afe28ea0cb3402b16ce541be7e5d1812a2
John Doe <johndoe@email.com>
2024-04-22 11:25:10 +0200

    cat

--- PathGitHub/Pathfinding/Script/Revew.js
+++ PathGitHub/Pathfinding/Script/Revew.js
@@ -17,11 +17,37 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+const url = `https://api.thecatapi.com/v1/images/search?limit=10`;
+const api_key = "DEMO_API_KEY"
+
+ fetch(url,{headers: {
+      'x-api-key': api_key
+    }})
+ .then((response) => {
+   return response.json();
+ })
+ .then((data) => {
+    let imagesData = data;
+    imagesData.map(function(imageData) {
+      
+      let image = document.createElement('img');
+      //use the url from the image object
+      image.src = `${imageData.url}`;
+          
+      let gridCell = document.createElement('div');
+      gridCell.classList.add('col');
+      gridCell.classList.add('col-lg');
+      gridCell.appendChild(image)
+        
+      document.getElementById('grid').appendChild(gridCell);
+      
+      });
+})
+.catch(function(error) {
+   console.log(error);
+});
 
 
 
 
-
-
-    
 });
